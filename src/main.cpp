c76aad93cede21e7315b19009228d55623218533
nikohonu <nikohonu@proton.me>
2024-12-10 16:44:02 +0200

    more opengl code for opengl god

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,21 @@
 #include <expected>
 #include <print>
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  std::println("{} {}", width, height);
+  glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
 auto app_main() -> std::expected<int, std::string> {
+  const auto width = 800;
+  const auto height = 600;
+
   // init glfw
   glfwInit();
   // setup major version of opengl >3<.3
@@ -14,7 +28,7 @@ auto app_main() -> std::expected<int, std::string> {
   // explicitly use the core-profile, without backwards-compatible features
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  auto window = glfwCreateWindow(width, height, "LearnOpenGL", NULL, NULL);
 
   if (window == NULL) {
     glfwTerminate();
@@ -26,6 +40,24 @@ auto app_main() -> std::expected<int, std::string> {
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     return std::unexpected("Failed to initialize GLAD");
   }
+
+  glViewport(0, 0, width, height);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    // process input
+    process_input(window);
+
+    // render
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // check events and swap buffers
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  glfwTerminate();
 
   return 0;
 }
